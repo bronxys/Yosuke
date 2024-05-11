0d1fe49a7a1af60ca7760f61c16d545fa9adaafc
Aleatory <josivalribeiro@gmail.com>
2024-05-10 21:53:08 -0300

    Atualizando..

--- start.sh
+++ start.sh
@@ -1,8 +1,14 @@
-#!bin/bash
+#!/bin/bash
 GREEN='\033[0;32m'
+
 while : 
+
 do
+
 echo "${GREEN}〔 么 Yosuke Bot 6.5 么 〕- Conexão Ativada para riscos de queda, aguarde..."
+
 node connect.js
+
 sleep 1
+
 done
